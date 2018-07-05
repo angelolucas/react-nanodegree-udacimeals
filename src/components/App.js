73c765ef3128ffdc17a697dcdb890c82b17a0cd9
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-05 11:25:15 -0300

    Using connect()

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
+    //console.log(this.props)
+
     return (
       <div>
-        Hello world!
+        {this.props.age}
       </div>
     )
   }
 }
-export default App
+
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: {...calendar[day]}
+    })),
+  }
+}
+
+export default connect(mapStateToProps)(App)
